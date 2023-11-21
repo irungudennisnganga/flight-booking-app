c55382d568349df963f374cb028659f31214152e
+dennisngangairungu <+>
2023-11-21 14:17:07 +0300

    added some styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import "./Card.css"
 function Card({ component}) {
-    console.log(component)
+    //console.log(component)
   return (
     <div className='card-holder'>
       {component.map( flight =>{
         return (
           <div className='card' key={flight.id}>
             <img className='image' src={flight.image} alt={flight.name} />
-            <h3>{flight.name}</h3>
-            <p>Depature : {flight.depature}</p>
-            <p>Arrival : {flight.arival}</p>
-            <p>Price : {flight.price}</p> 
+            <h3 className='card-title'>{flight.name}</h3>
+            <p> <b className='card-text'>Depature </b> : {flight.depature}</p>
+            <p> <b className='card-text'>Arrival</b>   : {flight.arival}</p>
+            <p> <b className='card-text'>Price</b>     : {flight.price}</p> 
+            <p> <b className='card-text'>Aeroplane </b>: {flight.aeroplane}</p>
           </div>
         )
       })}
