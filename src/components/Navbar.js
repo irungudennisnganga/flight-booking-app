88c649e582cd49d6bc1fb9875d71e7edd877a908
Abdi Cheda's MAC <mac@Abdi-Chedas-MacBook-Pro.local>
2023-11-22 12:36:04 +0300

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+      <nav>
+        <ul>
+          <li><a href='/flights'>Flights</a></li>
+          <li><a href='/bookings'>Bookings</a></li>
+          <li><a href='/tac'>Terms and Condition</a></li>
+        </ul>
+      </nav>
+    );
+  }
+  export default NavBar;
