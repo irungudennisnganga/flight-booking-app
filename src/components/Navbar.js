4e163edd5bb34310ad367455fb83f4493ce0b0b9
Abdi Cheda's MAC <mac@Abdi-Chedas-MacBook-Pro.local>
2023-11-22 13:02:04 +0300

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,8 @@ function NavBar() {
     return (
       <nav>
         <ul>
-          <li><a href='/flights'>Flights</a></li>
-          <li><a href='/bookings'>Bookings</a></li>
+          <li><a href='/flight'>Flight</a></li>
+          <li><a href='/booking'>Booking</a></li>
           <li><a href='/tac'>Terms and Condition</a></li>
         </ul>
       </nav>
