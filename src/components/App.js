fc9ce7c2e1e2ac0ff3f374b4bb58207aa4267acf
Abdi Cheda's MAC <mac@Abdi-Chedas-MacBook-Pro.local>
2023-11-22 16:05:33 +0300

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,28 @@
-import Flight from './Flight'
-import './App.css';
-import {Routes, Route} from "react-router-dom"
-import { useState,useEffect } from 'react';
-import Navbar from './Navbar'
-import Booking from './Booking'
-
-
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './Navbar.js';
+import Flights from './Flight';
+import Bookings from './Booking';
 
 function App() {
+  const [flights, setFlights] = useState([]);
 
-   const [flight,setFlight] = useState([])
-  const [load, setLoad]=useState(false)
+  useEffect(() => {
+    fetch('https://flights-api-512j.onrender.com/flights')
+      .then((response) => response.json())
+      .then((data) => setFlights(data))
+      .catch((error) => alert(error));
+  }, []);
 
-useEffect(() => {
-  fetch(`https://flights-api-512j.onrender.com/flights`)
-    .then((response) => response.json())
-    .then((data) =>{ setFlight(data)
-      setLoad(true)
-    })
-
-    .catch((error) => alert(error))
-    
-    
-},[])
-if(!load) {
-  return <div>
-    <div className="container">
-      <div className="cloud front">
-        <span className="left-front"></span>
-        <span className="right-front"></span>
-      </div>
-      <span className="sun sunshine"></span>
-      <span className="sun"></span>
-      <div className="cloud back">
-        <span className="left-back"></span>
-        <span className="right-back"></span>
-      </div>
-      </div>
-    </div>
-}
   return (
-    
-    <div className="App">
-     
-     <Navbar />
+    <>
+      <NavBar />
       <Routes>
-        <Route path='/' element={<Flight  flight={flight} />} />
-        <Route path='/flights' element={<Flight flights={flight} />} />
-        <Route path='/bookings' element={<Booking />} />
+        <Route path='/' element={<Flights flights={flights} />} />
+        <Route path='/flights' element={<Flights flights={flights} />} />
+        <Route path='/bookings' element={<Bookings />} />
       </Routes>
-    
-    </div>
+    </>
   );
 }
 
