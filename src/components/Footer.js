256358b107af890acc869ee7fcf4b5f587b3d48d
+dennisngangairungu <+>
2023-11-24 08:23:31 +0300

    changed image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,9 +8,10 @@ function Footer() {
         <img  className="logo" src="https://images.pexels.com/photos/733409/pexels-photo-733409.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Airplane Icon" />
       <div className="info">
         <h2>Contact Us</h2>
+        <p>Author : Hazel Njoki</p>
         <p>Author : Irungu Dennis</p>
         <p>Author : Abdi Cheda</p>
-        <p>Author : Hazel Njoki</p>
+       
       </div>
     </div>
   </div>
